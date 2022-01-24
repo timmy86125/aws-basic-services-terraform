a772ae318849e9038e60a57e40bc04e0aca49c89
timmy86125 <timmy86125@yahoo.com.tw>
2022-01-24 23:47:39 +0800

    add db subnets

--- vpc.tf
+++ vpc.tf
@@ -5,9 +5,10 @@ module "vpc" {
   name = "vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
-  private_subnets = ["10.0.10.0/24", "10.0.11.0/24"]
+  azs              = ["us-east-1a", "us-east-1b"]
+  public_subnets   = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnets  = ["10.0.10.0/24", "10.0.11.0/24"]
+  database_subnets = ["10.0.20.0/24", "10.0.21.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
